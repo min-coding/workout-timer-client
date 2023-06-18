61a5f1beba11910bcd14788fd9920dc8910f9644
Min <85483551+min-coding@users.noreply.github.com>
2023-06-19 00:23:20 +0700

    set hover

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import data from '../data/routinedata.json';
 
-function Sidebar({ chooseRoutine, isActive, setRoutineModal }) {
+function Sidebar({
+  chooseRoutine,
+  isActive,
+  setRoutineModal,
+  isHovered,
+  setIsHovered,
+}) {
   //call routine lists and save it and render here , if there's no routine suggest creating routine
   return (
     <div className="sidebar-container">
@@ -10,7 +16,18 @@ function Sidebar({ chooseRoutine, isActive, setRoutineModal }) {
         {data.map((routine) => {
           const { routine_id, routine_name } = routine;
           return (
-            <div key={routine_id} onClick={() => chooseRoutine(routine_id)}>
+            <div
+              key={routine_id}
+              className={`routine-link-container ${
+                isHovered === routine.routine_id ? 'hovered' : ''
+              }`}
+              onMouseOver={() => {
+                setIsHovered(routine_id);
+              }}
+              onMouseLeave={() => {
+                setIsHovered(0);
+              }}
+              onClick={() => chooseRoutine(routine_id)}>
               <Link
                 to={`/plan/${routine_id}`}
                 className={isActive === routine.routine_id ? 'active' : ''}>
